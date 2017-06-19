67bcc439d742d6c8557d46ec5cb78490453bf83e
Jakob Alander <jakob.alander@gmail.com>
2017-06-19 06:29:51 +0200

    Put all pieces together

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import com.google.common.base.Stopwatch;
+
 import java.util.TreeSet;
 
 /**
@@ -5,6 +7,20 @@ import java.util.TreeSet;
  */
 public class Main {
     public static void main(String[] args) {
+        String str = "1276543";
+        int primes = 0;
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        TreeSet<String> perms = new Permutations().createPermuations(str);
 
+        System.out.println(stopwatch.elapsed());
+        stopwatch.reset();
+        stopwatch.start();
+        for(String s : perms) {
+            if(Prime.isPrime(Integer.parseInt(s))) {
+                primes++;
+            }
+        }
+        System.out.println(stopwatch.elapsed());;
+        System.out.println("Number of primes " + primes);
     }
 }
